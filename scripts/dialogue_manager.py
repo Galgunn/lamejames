d4f3c47eb4b40894c31aa3687502086dc733a1c2
Galgunn <gtzkm18@gmail.com>
2026-05-29 00:45:49 -0400

    ver 0.0.13 added new python and json files

---
+++ scripts/dialogue_manager.py
@@ -0,0 +1,41 @@
+class DialogueManager:
+    def __init__(self, game):
+        self.game = game
+
+    def check_conditions(self, data):
+        current_flags = self.game.flags
+        data_flags = data.get('requires', []) # Return empty list if no requires key is found
+
+        for req_flag in data_flags: # If data flags is empty list you will not enter for loop
+            if req_flag not in current_flags:
+                return False
+        return True
+    
+    def select_interaction(self, dialogue_json):
+        valid = []
+
+        for key, data in dialogue_json['interactions'].items(): # Tuple unpacking
+            repeatable = data.get('repeatable', True) # Return True if no repeatable value is found
+
+            if repeatable == False:
+                if key in self.game.used_interactions:
+                    continue
+
+            if self.check_conditions(data):
+                valid.append((key, data))
+        
+        if not valid: # if valid is not an empty list
+            return None
+        
+        # Picks the highest priority data from the list of valid dialogues
+        best_key, best_data = max(valid, key=lambda x: x[1].get('priority', 0))
+        return best_key, best_data
+    
+# example: valid = [('intro', {'priority': 0, ...}), ('repeat', {'priority': 1, ...})]
+# key is an optional argument in the max() function which lets you modify an object before comparison or compare based on specific index/attribute
+# if we just do max(valid) it would look at each tuple at index 1 and get the largest alpabetically ordered string
+# what we need is the max priority number in the data for that we use the key= argument
+# lambda is a one line anonymous function 
+# what were doing when we do key=lambda x: x[1].get('priority', 0) is...
+# ('intro', {'priority': 0, ...}) take this tuple and go to index 1 which is our data and return the priority value/num. If its not found return 0 
+# finally get the largest of the priority numbers we got.
